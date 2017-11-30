c472b7b899465aecb958bc7b1b440327cf0bdf3f
Raychan <davidrueckert@hotmail.de>
2017-11-30 21:26:50 +0100

    Initial commit

---
+++ LuefterSteuerung/main.c
@@ -0,0 +1,16 @@
+/*
+ * LuefterSteuerung.c
+ *
+ * Created: 30.11.2017 21:20:53
+ * Author : Raychan
+ */ 
+
+#include <avr/io.h>
+
+
+/* Replace with your library code */
+int myfunc(void)
+{
+	return 0;
+}
+
