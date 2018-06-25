71781a18f1b08990defea2973fc6633feee67b19
Raychan <davidrueckert@hotmail.de>
2018-06-25 09:24:55 +0200

    Namensänderungen

--- LuefterSteuerung/temp_mcp9700.h
+++ LuefterSteuerung/temp_mcp9700.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 Temperatur Funktion
 *********************************************************************************
-File: temp.h
-Version: 1.0
+File: temp_mcp9700.h
+Version: 1.1
 Author: RayChan
 Beschreibung:
 
@@ -16,8 +16,8 @@ Hier wird der Temperaturfühler MCP9700 verwendet
 //	Header-Definition:
 // ----------------------------------------------------------------------------//
 
-#ifndef TEMP_H_
-	#define TEMP_H_
+#ifndef TEMP_MCP9700_H_
+	#define TEMP_MCP9700_H_
 
 // ----------------------------------------------------------------------------//
 //	Header-Dateien:
@@ -29,21 +29,21 @@ Hier wird der Temperaturfühler MCP9700 verwendet
 // 	Definition (Einstellungen):
 // ----------------------------------------------------------------------------//
 
-	#define TEMP_DDR		DDRB		//DDR festlegen
-	#define TEMP_PORT		PORTB		//Port festlegen
-	#define TEMP_PIN		PB4		//Pin festlegen
-	#define REF_MODI		AVCC_REFERENZ	//Referenz Modus wählen
-    	#define VREF            5                   	//Referenz Spannung
+	#define TEMP_MCP_DDR		DDRB		//DDR festlegen
+	#define TEMP_MCP_PORT		PORTB		//Port festlegen
+	#define TEMP_MCP_PIN		PB4		//Pin festlegen
+	#define MCP_REF_MODI		AVCC_REFERENZ	//Referenz Modus wählen
 	//AREF_Referenz; AVCC_Referenz; Intern_Referenz
-
+    	#define MCP_VREF	        5             	//Referenz Spannung
+	
 // ----------------------------------------------------------------------------//
 // 	Funktions Prototypen:
 // ----------------------------------------------------------------------------//
 
-	extern void temp_init(void);			//Temperaturmessung initalisierung und ADC starten
-	extern float temp(void);			//Gibt den Fertig berechneten Temperaturwert in Float zurück
+	extern void temp_mcp_init(void);		//Temperaturmessung initalisierung und ADC starten
+	extern float temp_mcp(void);			//Gibt den Fertig berechneten Temperaturwert in Float zurück
 
 // ----------------------------------------------------------------------------//
 // 	Ende
 // ----------------------------------------------------------------------------//
-#endif /* TEMP_H_ */
+#endif /* TEMP_MCP9700_H_ */
